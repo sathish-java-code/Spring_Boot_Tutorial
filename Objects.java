db96af26bb167f625aeeecaa7a9b98bc9394e199
sathish-java-code <sathishmadhu@hotmail.com>
2023-06-26 18:41:26 +0530

    firstcommit

---
+++ Objects.java
@@ -0,0 +1,15 @@
+public class Objects{
+
+
+    public void hello(){
+        System.out.println("hello spring boot tutorial");
+    }
+    public static void main(String[] args) {
+
+        Objects o=new Objects();
+        o.hello();
+         
+         
+     }
+
+}
